16ad0871cae11d09711e0087bc8f4ce53e69dada
Prajwal Shrestha <pjmessi25@icloud.com>
2023-09-01 21:55:08 +0545

    Add gorm config in User

--- internal/pkg/model/user.go
+++ internal/pkg/model/user.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type User struct {
-	Id        string
-	FirstName *string
-	LastName  *string
-	Email     string
-	CreatedAt time.Time
-	UpdatedAt *time.Time
+	Id        string     `gorm:"primaryKey;column:id"`
+	FirstName *string    `gorm:"column:first_name"`
+	LastName  *string    `gorm:"column:last_name"`
+	Email     string     `gorm:"column:email"`
+	CreatedAt time.Time  `gorm:"column:created_at"`
+	UpdatedAt *time.Time `gorm:"column:updated_at"`
 }
