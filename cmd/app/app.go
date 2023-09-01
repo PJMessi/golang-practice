97c420dc7353c46cdec7db07642569e4899fc310
Prajwal Shrestha <pjmessi25@icloud.com>
2023-09-01 21:51:09 +0545

    Remove multiple .env load

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,19 +1,12 @@
 package app
 
 import (
-	"log"
-
-	"github.com/joho/godotenv"
 	"github.com/pjmessi/go-database-usage/config"
 	"github.com/pjmessi/go-database-usage/internal/pkg/db"
 )
 
 func StartApp() {
 	appConfig := config.GetAppConfig()
-
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
 
 	var dbInstance db.Db = db.CreateDbMysql()
 	dbInstance.InitializeConnection(appConfig)
